245adac7c079bdb9cdf52edf5ea1b674500792ff
Ailton Pantoja <69184378+AiltonPantoja@users.noreply.github.com>
2020-10-08 10:30:09 -0300

    adicionar arquivos para request

---
+++ painelcovidap/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Casos from "./components/Casos"
+function App() {
+  return (
+    <div className="App">
+      <Casos />
+
+    </div>
+  );
+}
+
+export default App;
